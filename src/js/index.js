bd25cc9af2b1d6266195f1094d278af46da1e23c
NicoB <nicobasma_@hotmail.com>
2023-02-23 15:48:41 +0000

    navbar super chula

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Navbar from "./Navbar.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar />, document.querySelector("#app"));
