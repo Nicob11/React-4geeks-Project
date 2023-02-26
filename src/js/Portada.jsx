46f599ff35b950c10c924d56cdd43255aba02a89
NicoB <nicobasma_@hotmail.com>
2023-02-26 10:36:41 +0000

    add a button to create a new card

--- src/js/Portada.jsx
+++ src/js/Portada.jsx
@@ -10,16 +10,31 @@ import Footer from "./Footer.jsx";
 const Portada = () =>{
     
     const [temporalCards, setTemporalCards] = useState(cartas);
+    let count = 4;
     const handleDeleteCard = (card) =>{ 
-           
+        
         const newArray = temporalCards.filter((carta) => carta.id !== card.id);
         setTemporalCards(newArray)
+    }
+    const handleNewCard = () =>{
+        count++;
+        let newCard = [...temporalCards]
+        const otherCard = {
+            img:`https://i.imgur.com/zXGdRb3.jpg`,
+            title: `Card`,
+            body:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo numquam consectetur rerum minus ab velit architecto aliquam ducimus exercitationem id assumenda molestias neque omnis cum ipsum tempora impedit, aliquid odio.`,
+            id:{count}
+        
+        }
+        console.log('hello',count)
+        newCard.push(otherCard)
+        setTemporalCards(newCard)
     }
     return (
         <>
             <Navbar/>
             <div className="container">
-            <Jumbotron/>
+            <Jumbotron handleNewCard={handleNewCard}/>
             <div className="row p-0">
             {temporalCards.map((carta) => <Cards handleDeleteCard={handleDeleteCard} id={carta.id} key={carta.id} src={carta.img} title={carta.title} body={carta.body}/>)}
             </div>
--- src/js/cartas.jsx
+++ src/js/cartas.jsx
@@ -1,26 +1,26 @@
 export const cartas = [{
-    img:`rigo-baby.jpg`,
-    title: `hello world`,
-    body:`Someting`,
+    img:`https://i.imgur.com/zXGdRb3.jpg`,
+    title: `Card`,
+    body:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo numquam consectetur rerum minus ab velit architecto aliquam ducimus exercitationem id assumenda molestias neque omnis cum ipsum tempora impedit, aliquid odio.`,
     id:1
 
 },
 {
-    img:`rigo-baby.jpg`,
-    title: `hello world`,
-    body:`Someting`,
+    img:`https://i.imgur.com/zXGdRb3.jpg`,
+    title: `Card`,
+    body:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo numquam consectetur rerum minus ab velit architecto aliquam ducimus exercitationem id assumenda molestias neque omnis cum ipsum tempora impedit, aliquid odio.`,
     id:2
 },
 {
-    img:`rigo-baby.jpg`,
-    title: `hello world`,
-    body:`Someting`,
+    img:`https://i.imgur.com/zXGdRb3.jpg`,
+    title:`Card`,
+    body:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo numquam consectetur rerum minus ab velit architecto aliquam ducimus exercitationem id assumenda molestias neque omnis cum ipsum tempora impedit, aliquid odio.`,
     id:3
 },
 {
-    img:`rigo-baby.jpg`,
-    title: `hello world`,
-    body:`Someting`,
+    img:`https://i.imgur.com/zXGdRb3.jpg`,
+    title: `Card`,
+    body:`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo numquam consectetur rerum minus ab velit architecto aliquam ducimus exercitationem id assumenda molestias neque omnis cum ipsum tempora impedit, aliquid odio.`,
     id:4
 }
 ]
